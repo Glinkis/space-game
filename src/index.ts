f0a2c195a48d22670d001c437191227137e7fae0
Victor Glindås <glinkis@gmail.com>
2020-11-05 18:51:44 +0100

    Set up build tools

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const name = "Kenobi"
+const title = "general"
+
+console.log(`Hello there, ${title} ${name}.`)
+
+export { name }
---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import { Configuration } from "webpack"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import * as packageConfig from "./package.json"
+
+export default (): Configuration => ({
+  module: {
+    rules: [
+      {
+        test: /ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: packageConfig.name,
+    }),
+  ],
+})
