07d56a9ce064f3d0bebe0dd6f78962a0caaa7248
Victor Glindås <glinkis@gmail.com>
2020-11-05 19:50:34 +0100

    Set up viewport

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import * as PIXI from "pixi.js"
+import { Viewport } from "pixi-viewport"
 import "./styles.css"
 
 const app = new PIXI.Application({
   resizeTo: window,
 })
-
 document.body.appendChild(app.view)
+
+const viewport = new Viewport()
+app.stage.addChild(viewport)
+
+viewport.drag().pinch().wheel().decelerate({ friction: 0.7 })
+
+const textStyle = new PIXI.TextStyle({
+  fontFamily: "Arial",
+  fontSize: 64,
+  fill: "white",
+  strokeThickness: 4,
+})
+
+const message = new PIXI.Text("Hey!", textStyle)
+
+app.ticker.add(() => {
+  message.text = app.ticker.lastTime.toFixed(0)
+})
+
+viewport.addChild(message)
